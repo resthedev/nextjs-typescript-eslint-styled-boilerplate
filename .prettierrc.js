2d5ae8ff97bae61cce808e58f8ff167e5e5b52f6
goss <prodbygoss@gmail.com>
2021-06-25 23:35:23 -0700

    edits

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,4 @@
-// .prettierrc.js
 module.exports = {
-  // Change your rules accordingly to your coding style preferences.
-  // https://prettier.io/docs/en/options.html
   semi: false,
   trailingComma: 'es5',
   singleQuote: true,
