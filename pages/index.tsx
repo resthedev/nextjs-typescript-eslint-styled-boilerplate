9c5920104b776b76df6c1497b8ca2fc384535906
goss <prodbygoss@gmail.com>
2021-06-26 00:03:30 -0700

    adjust more

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,9 @@
-import { GetServerSideProps, NextPage } from 'next'
+import { NextPage } from 'next'
 import styled from 'styled-components'
 
-interface Props {
-  launch: {
-    mission: string
-    site: string
-    timestamp: number
-    rocket: string
-  }
-}
-const IndexPage: NextPage<Props> = ({ launch }) => {
-  const date = new Date(launch.timestamp)
-  return (
-    <main>
-      <h1>Next SpaceX Launch: {launch.mission}</h1>
-      <p>
-        {launch.rocket} will take off from {launch.site} on {date.toDateString()}
-      </p>
-    </main>
-  )
+const Container = styled.div``
+
+const IndexPage: NextPage = () => {
+  return <Container>hit here</Container>
 }
 export default IndexPage
-
-/*
- * More information about getServerSideProps:
- * https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
- */
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const response = await fetch('https://api.spacexdata.com/v3/launches/next')
-  const nextLaunch = await response.json()
-  return {
-    props: {
-      launch: {
-        mission: nextLaunch.mission_name,
-        site: nextLaunch.launch_site.site_name_long,
-        timestamp: nextLaunch.launch_date_unix * 1000,
-        rocket: nextLaunch.rocket.rocket_name,
-      },
-    },
-  }
-}
